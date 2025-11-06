8562ee8b1ef7580f2df21d8451208c077044ab6b
NeXus1183 <99423113+NeXus1183@users.noreply.github.com>
2025-11-06 22:12:51 +0800

    test

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 function switchPage(event)
 {
     alert("body clicked");
-    document.location.pathname = "../pages/home.html";
+    window.location.href = "../pages/home.html";
     console.log("Test");
 }
 
