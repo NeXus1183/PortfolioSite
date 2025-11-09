0ccf4722bf8ac9963469f8bf686d85958b6212c7
NeXus1183 <99423113+NeXus1183@users.noreply.github.com>
2025-11-10 02:25:32 +0800

    Added resume and projects pages

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 function switchPage(event)
 {
+    alert("body clicked");
+    window.location.href = "../pages/home.html";
     console.log("Test");
 }
 
---
+++ scripts/projects.js
@@ -0,0 +1,20 @@
+//const overlay = document.getElementById("over");
+//const about = document.getElementById("about");
+//const note = document.getElementById("note");
+const test = document.getElementById("oM");
+
+//note.addEventListener("click", event => {
+//    overlay.classList.add("active");
+//    about.classList.add("active");
+//})
+
+//overlay.addEventListener("click", event => {
+//    overlay.classList.remove("active");
+//    about.classList.remove("active");
+//})
+
+test.addEventListener("click", event => {
+    console.log("TESTTTTTTT");
+    alert("div clicked");
+})
+
